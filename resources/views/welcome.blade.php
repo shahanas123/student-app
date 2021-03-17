a5a01c6b9f937a94d05389b2ede83e320b541de7
shahanas123 <mca545.rajagiri.edu>
2021-03-17 11:57:43 +0530

    push

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<div class="container">
+    <div class="row">
+    <div class="col col-12 col-sm-4 col-md-4 col-lg-4 col-xl-4 col-xxl-4"></div>
+        <div class="col col-12 col-sm-4 col-md-4 col-lg-4 col-xl-4 col-xxl-4">
+        <centre><h3>REGISTRATION</h3></centre>
+        <table class="table">
+        <tr>
+            <td>Name</td>
+            <td><input type="text" class="form-control"></td>
+        </tr>
+        <tr>
+        <td>Roll no</td>
+            <td><input type="text" class="form-control"></td>
+        </tr>
+        <tr>
+        <td>Admission No</td>
+            <td><input type="text" class="form-control"></td>
+        </tr>
+        <tr>
+        <td>College</td>
+            <td><input type="text" class="form-control"></td>
+        </tr>
+        <tr>
+        <td></td>
+            <td><button class="btn btn-primary">submit</button></td>
+        </tr></table>
+        </div>
+    </div>
+</div>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.bundle.min.js" integrity="sha384-b5kHyXgcpbZJO/tY9Ul7kGkf1S0CWuKcCD38l8YkeH8z8QjE0GmW1gYU5S9FOnJ0" crossorigin="anonymous"></script>   
+</body>
+</html>
